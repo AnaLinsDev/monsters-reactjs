21ea376929a7aec555e64466b27f6369cf92ef8a
AnaJL <analinsdev@gmail.com>
2020-05-19 17:40:52 -0300

    Updates

---
+++ precache-manifest.ef9cc0d73194ec2d1a7b889736a0f4b4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "90abc2113adf634c6e8e00e670932e77",
+    "url": "/monsters-reactjs/index.html"
+  },
+  {
+    "revision": "2d6c4e53866fb059965e",
+    "url": "/monsters-reactjs/static/css/main.93fd95a2.chunk.css"
+  },
+  {
+    "revision": "12240f73676d8f2fbd85",
+    "url": "/monsters-reactjs/static/js/2.a3c82928.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-reactjs/static/js/2.a3c82928.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2d6c4e53866fb059965e",
+    "url": "/monsters-reactjs/static/js/main.32601a1e.chunk.js"
+  },
+  {
+    "revision": "af403446851106141fb8",
+    "url": "/monsters-reactjs/static/js/runtime-main.375f121d.js"
+  }
+]);
